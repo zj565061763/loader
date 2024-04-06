7dc86731165ae33f01f25a6188dfd1eb46c8767d
zjun <565061763@qq.com>
2024-04-07 00:05:22 +0800

    test

--- app/src/test/java/com/sd/demo/loader/LoaderTest.kt
+++ app/src/test/java/com/sd/demo/loader/LoaderTest.kt
@@ -1,6 +1,8 @@
 package com.sd.demo.loader
 
 import com.sd.lib.loader.FLoader
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -49,6 +51,22 @@ class LoaderTest {
         } catch (e: Exception) {
             assertEquals("failure", e.message)
         }
+    }
+
+    @Test
+    fun `test load cancel`(): Unit = runBlocking {
+        val loader = FLoader()
+
+        val job = launch {
+            loader.load {
+                delay(2_000)
+                1
+            }
+        }
+
+        delay(1_000)
+        loader.cancelLoad()
+        assertEquals(true, job.isCancelled)
     }
 
     @Test
