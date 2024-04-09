af789f008cb1aad05f77df989df059f858bf5a3a
zjun <565061763@qq.com>
2024-04-09 14:05:07 +0800

    loadOrThrow

--- lib/src/main/java/com/sd/lib/loader/FLoader.kt
+++ lib/src/main/java/com/sd/lib/loader/FLoader.kt
@@ -28,6 +28,21 @@ interface FLoader {
     ): Result<T>
 
     /**
+     * 开始加载，如果上一次加载还未完成，再次调用此方法，会取消上一次加载。
+     * 如果[onStart]触发了，则[onFinish]一定会触发。
+     * [onStart],[onLoad]的异常会被抛出。
+     *
+     * @param onStart 开始回调
+     * @param onFinish 结束回调
+     * @param onLoad 加载回调
+     */
+    suspend fun <T> loadOrThrow(
+        onStart: suspend () -> Unit = {},
+        onFinish: () -> Unit = {},
+        onLoad: suspend () -> T,
+    ): T
+
+    /**
      * 取消加载
      */
     suspend fun cancelLoad()
@@ -48,7 +63,7 @@ private class LoaderImpl : FLoader {
         onStart: suspend () -> Unit,
         onFinish: () -> Unit,
         onFailure: (Throwable) -> Unit,
-        onLoad: suspend () -> T
+        onLoad: suspend () -> T,
     ): Result<T> {
         return _mutator.mutate {
             try {
@@ -62,6 +77,22 @@ private class LoaderImpl : FLoader {
                 if (e is CancellationException) throw e
                 onFailure(e)
                 Result.failure(e)
+            } finally {
+                onFinish()
+            }
+        }
+    }
+
+    override suspend fun <T> loadOrThrow(
+        onStart: suspend () -> Unit,
+        onFinish: () -> Unit,
+        onLoad: suspend () -> T,
+    ): T {
+        return _mutator.mutate {
+            try {
+                onStart()
+                currentCoroutineContext().ensureActive()
+                onLoad()
             } finally {
                 onFinish()
             }
