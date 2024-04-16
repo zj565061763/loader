68efa456087bfb7ccfc02c068e85dbe84568caa9
zjun <565061763@qq.com>
2024-04-16 09:05:28 +0800

    comment

--- lib/src/main/java/com/sd/lib/loader/FDataLoader.kt
+++ lib/src/main/java/com/sd/lib/loader/FDataLoader.kt
@@ -22,7 +22,7 @@ interface FDataLoader<T> {
     val dataFlow: Flow<T>
 
     /**
-     * 开始加载，如果上一次加载还未完成，再次调用此方法，会取消上一次加载
+     * 开始加载，如果上一次加载还未完成，再次调用此方法，会取消上一次加载([CancellationException])。
      *
      * @param notifyLoading 是否通知[DataState.isLoading]
      * @param onLoad 加载回调
